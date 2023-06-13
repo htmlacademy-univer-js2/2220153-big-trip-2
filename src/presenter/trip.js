589755dae2e8b3fd335184d6ddb9cb0da5caa489
nedluvq <100476389+nedluvq@users.noreply.github.com>
2023-06-13 15:09:08 +0500

    321312

--- src/presenter/trip.js
+++ src/presenter/trip.js
@@ -1,90 +1,61 @@
-import TripList from '../view/trip-list.js';
-import Point from '../view/point.js';
-import Sort from '../view/sort.js';
+import TripEventsView from '../view/events-view.js';
+import RoutePointView from '../view/point-edit-view.js';
+import FormEditView from '../view/form-edit.js';
 import { render } from '../render.js';
-import PointNull from '../view/point-null.js';
-import PointEdit from '../view/point-edit.js';
 
 export default class TripEventsPresenter {
-  #eventsList = null;
-  #tripContainer = null;
-  #pointsModel = null;
-  #boardPoints = null;
-  #destinations = null;
-  #offers = null;
-  constructor(tripContainer) {
-    this.#eventsList = new TripList();
-    this.#tripContainer = tripContainer;
+  constructor() {
+    this._rootContainer = null;
+    this._eventsModel = null;
+    this._events = null;
+    this._eventList = new TripEventsView();
   }
 
-  init(pointsModel) {
-    this.#pointsModel = pointsModel;
-    this.#boardPoints = [...this.#pointsModel.points];
-    this.#destinations = [...this.#pointsModel.destinations];
-    this.#offers = [...this.#pointsModel.offers];
+  _renderEvent(event) {
+    const eventComponent = new RoutePointView(event);
+    const eventEditComponent = new FormEditView(event);
 
 
-    if(this.#boardPoints.length === 0) {
-      render(new PointNull(), this.#tripContainer);
-    } else {
-      render(new Sort(), this.#tripContainer);
-      render(this.#eventsList, this.#tripContainer);
-      for (const point of this.#boardPoints) {
-        this.#renderPoints(point);
-      }
-    }
-
-  }
-
-  #renderPoints = (point) => {
-    const pointPreviewComponent = new Point(point, this.#destinations, this.#offers);
-    const editingPointComponent = new PointEdit(point, this.#destinations, this.#offers);
-
-    const replacePointToEditForm = () => {
-      this.#eventsList.element.replaceChild(
-        editingPointComponent.element,
-        pointPreviewComponent.element
-      );
+    const editToEvent = () => {
+      this._eventList.element.replaceChild(eventComponent.element, eventEditComponent.element);
     };
 
-    const replaceEditFormToPoint = () => {
-      this.#eventsList.element.replaceChild(
-        pointPreviewComponent.element,
-        editingPointComponent.element
-      );
+    const eventToEdit = () => {
+      this._eventList.element.replaceChild(eventEditComponent.element, eventComponent.element);
     };
 
     const onEscKeyDown = (evt) => {
       if (evt.key === 'Escape' || evt.key === 'Esc') {
         evt.preventDefault();
-        replaceEditFormToPoint();
+        editToEvent();
         document.removeEventListener('keydown', onEscKeyDown);
       }
     };
 
-    pointPreviewComponent.element
-      .querySelector('.event__rollup-btn')
-      .addEventListener('click', () => {
-        replacePointToEditForm();
-        document.addEventListener('keydown', onEscKeyDown);
-      });
+    eventComponent.element.querySelector('.event__rollup-btn').addEventListener('click', () => {
+      eventToEdit();
+      document.addEventListener('keydown', onEscKeyDown);
+    });
 
-    editingPointComponent.element
-      .querySelector('.event__rollup-btn')
-      .addEventListener('click', (evt) => {
-        evt.preventDefault();
-        replaceEditFormToPoint();
-        document.removeEventListener('keydown', onEscKeyDown);
-      });
+    eventEditComponent.element.querySelector('.event__rollup-btn').addEventListener('click', () => {
+      editToEvent();
+      document.removeEventListener('keydown', onEscKeyDown);
+    });
 
-    editingPointComponent.element
-      .querySelector('form')
-      .addEventListener('submit', (evt) => {
-        evt.preventDefault();
-        replaceEditFormToPoint();
-        document.removeEventListener('keydown', onEscKeyDown);
-      });
+    eventEditComponent.element.querySelector('.event__save-btn').addEventListener('click', (evt) => {
+      evt.preventDefault();
+      editToEvent();
+      document.removeEventListener('keydown', onEscKeyDown);
+    });
 
-    render(pointPreviewComponent, this.#eventsList.element);
+    render(eventComponent, this._eventList.element);
+  }
+
+  init(tripContainer, pointsModel) {
+    this._rootContainer = tripContainer;
+    this._eventsModel = pointsModel;
+    this._events = [...this._eventsModel.events];
+    render(this._eventList, this._rootContainer);
+    this._events.forEach((event) => this._renderEvent(event));
   }
 }
